72c3f012c95ba946880ecc099a4326575d012e18
Issei Nagi <issei.nagi@gmail.com>
2021-08-17 12:04:41 +0900

    404エラーハンドリング

--- FunctionProject/urls.py
+++ FunctionProject/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from FunctionApp.views import show_error_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FunctionApp.urls')),
     path('boards/', include('boards.urls')),
 ]
+
+handler404 = show_error_page
